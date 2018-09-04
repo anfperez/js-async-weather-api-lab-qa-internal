f47012c6b145f813a509f502fc0a206a5dcc8d88
Angela Perez <angela.perez@flatironschool.com>
2018-09-04 23:25:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,116 @@
-const API_KEY = "YOUR API KEY"
+const API_KEY = "6549863730776a52fadf3fe935d5eecc";
+
+function handleFormSubmit(event) {
+  event.preventDefault()
+  const input = document.getElementById("city")
+  const value = input.value
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=` + value + `&APPID=${API_KEY}&units=imperial`)
+  .then(response => response.json())
+  .then(responseJSON => displayCurrentWeather(responseJSON))
+  fetchCurrentWeather(value)
+  fetchFiveDayForecast(value)
+  }
+
+function displayCurrentWeather(json) {
+  //current temp, low, hi, humidity, cloud cloudCover
+  const mainData = json.main
+  const temp = document.getElementById("temp")
+  const low = document.getElementById("low")
+  const high = document.getElementById("high")
+  const humidity = document.getElementById("humidity")
+  const cloudCover = document.getElementById("cloudCover")
+  
+  temp.innerHTML = mainData.temp
+  low.innerHTML = mainData.temp_min
+  high.innerHTML = mainData.temp_max
+  humidity.innerHTML = mainData.humidity
+  cloudCover.innerHTML = json.clouds.all
+
+}
+
+function fetchCurrentWeather(city) {}
+
+function fetchFiveDayForecast(city) {
+  fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${API_KEY}&units=imperial&cnt=40`)
+  .then(response => response.json())
+  .then(responseJSON => {displayFiveDayForecast(responseJSON)
+  return responseJSON
+  })
+  .then(responseJSON => createChart(responseJSON))
+}
+
+function displayFiveDayForecast(json) {
+  console.log("putting here")
+  const forecast = json.list
+  const aside = document.querySelector("aside")
+  let counter = 0
+  let lowAverageTemp = 0
+  let highAverageTemp = 0
+  forecast.forEach((eachForecastItem) => {
+    counter++
+    lowAverageTemp += eachForecastItem.main.temp_min
+    highAverageTemp += eachForecastItem.main.temp_max
+    if (counter === 8) {
+      counter = 0
+      const div = document.createElement("div")
+      div.innerHTML = `<p>${eachForecastItem.dt_txt}</p> <p>${Math.floor(lowAverageTemp / 8)}</p> <p>${Math.floor(highAverageTemp / 8) }</p>`
+      lowAverageTemp = 0 
+      highAverageTemp = 0 
+      aside.appendChild(div)
+    }
+  })
+}
+
+function createChart(json) { 
+  const ctx = document.getElementById("WeatherChart").getContext("2d")
+  const labels = json.list.map((increment) => increment.dt_txt) //dt_txt
+  console.log(labels)
+  const data = json.list.map((increment) => increment.main.temp)
+  var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: labels,
+        datasets: [{
+            label: 'forecast',
+            data: data,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+  
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('cityForm').addEventListener('submit', handleFormSubmit)
+});
+
+//official answer below
+/*const API_KEY = "YOUR API KEY"
 
 function handleFormSubmit(event) {
   event.preventDefault()
@@ -103,3 +215,4 @@ document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('cityForm').addEventListener('submit', handleFormSubmit)
   document.getElementById('city').focus()
 })
+*/
